3c98ea761fb4014c89781fdbf0c34c571ee67a3d
German Macias <germanmac89@gmail.com>
2023-04-05 13:17:22 -0700

    initial commit

---
+++ for_loop_basic1.py
@@ -0,0 +1,35 @@
+#for i in range(151):
+    #print(i)
+
+#for i in range(5, 1000, 5):
+    #print(i)
+
+#for i in range(1, 101):
+    #print(i)
+
+count = 0
+while count <= 100:
+    print(count)
+    count+=1
+if count % 5:
+    print("coding")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#for i in range (2018,0,-4):
+    #print(i)
+
+
+
+
