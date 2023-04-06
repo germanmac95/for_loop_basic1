7c90524e0bda34fb78ef71e1a3be52c11aa05853
German Macias <germanmac89@gmail.com>
2023-04-06 16:31:48 -0700

    uncommented the code lol

--- for_loop_basic1.py
+++ for_loop_basic1.py
@@ -1,8 +1,8 @@
-"""for i in range(151):
+for i in range(151):
     print(i)
 
 for i in range(5, 1000, 5):
-    print(i)"""
+    print(i)
 
 for i in range(101):
     if i % 10 == 0:
@@ -13,12 +13,12 @@ for i in range(101):
         continue
     print(i)
 
-"""sum = 0
+sum = 0
 for i in range( 1, 500000, 2):
     sum = sum + i
-print(f"sum is {sum}")"""
+print(f"sum is {sum}")
 
-"""for i in range (2018,0,-4):
+for i in range (2018,0,-4):
     print(i)
 
 low_num = 2
@@ -26,5 +26,5 @@ high_num = 9
 mult = 3
 for i in range(low_num, high_num + 1):
     if i % mult == 0:
-        print(i)"""
+        print(i)
 
