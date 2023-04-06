0e91ee5d37d6be78d787eabbe034ed2e8ac0bc2f
German Macias <germanmac89@gmail.com>
2023-04-06 16:26:57 -0700

    finished assignment

--- for_loop_basic1.py
+++ for_loop_basic1.py
@@ -1,35 +1,30 @@
-#for i in range(151):
-    #print(i)
+"""for i in range(151):
+    print(i)
 
-#for i in range(5, 1000, 5):
-    #print(i)
+for i in range(5, 1000, 5):
+    print(i)"""
 
-#for i in range(1, 101):
-    #print(i)
+for i in range(101):
+    if i % 10 == 0:
+        print("coding dojo")
+        continue
+    if i % 5 == 0:
+        print("coding")
+        continue
+    print(i)
 
-count = 0
-while count <= 100:
-    print(count)
-    count+=1
-if count % 5:
-    print("coding")
+"""sum = 0
+for i in range( 1, 500000, 2):
+    sum = sum + i
+print(f"sum is {sum}")"""
 
+"""for i in range (2018,0,-4):
+    print(i)
 
-
-
-
-
-
-
-
-
-
-
-
-
-#for i in range (2018,0,-4):
-    #print(i)
-
-
-
+low_num = 2
+high_num = 9
+mult = 3
+for i in range(low_num, high_num + 1):
+    if i % mult == 0:
+        print(i)"""
 
